207a5fd8571bf7a5fb31ddb9d3df5732eb4021c7
M <mrgmckenzie@gmail.com>
2023-04-21 13:34:47 +0100

    first commit

---
+++ models/library.py
@@ -0,0 +1,9 @@
+from book import Book
+
+first_book = Book("Frankenstein", "Mary Shelley", "Horror")
+second_book = Book("Dracula", "Bram Stoker", "Horror")
+third_book = Book("War and Peace", "Leo Tolstoy", "History")
+fourth_book = Book("A Tale of Two Cities", "Charles Dickens", "Comedy")
+fifth_book = Book("It", "Steven King", "Horror")
+
+library = [first_book, second_book, third_book, fourth_book, fifth_book]
