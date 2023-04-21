d3716917fd6cc97b07eb7bb54b9e41c96e9ea0ad
M <mrgmckenzie@gmail.com>
2023-04-21 19:57:35 +0100

    added single book view

--- controllers/controller.py
+++ controllers/controller.py
@@ -5,4 +5,13 @@ from models.library import books_in_library
 
 @app.route('/library')
 def index():
-    return render_template('index.jinja', library_books = books_in_library)
+    return render_template('index.jinja', library_book_list = books_in_library)
+
+@app.route('/library/<title>')
+def individual_book(title):
+    chosen_book = []
+    for book in books_in_library:
+        if book.title == title:
+            chosen_book.append(book)
+    
+    return render_template('index.jinja', library_book_list = chosen_book)
