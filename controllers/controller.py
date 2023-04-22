d70a42c2d98655a4889ffa27658c663d4f14bfb1
M <mrgmckenzie@gmail.com>
2023-04-22 20:05:03 +0100

    created 'add book to library' feature

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
+from models.book import Book
 
 from models.library import books_in_library
 
@@ -15,3 +16,14 @@ def individual_book(title):
             chosen_book.append(book)
     
     return render_template('index.jinja', library_book_list = chosen_book)
+
+@app.route('/library', methods=['POST'])
+def add_book():
+    book_title = request.form['title']
+    book_author = request.form['author']
+    book_genre = request.form['genre']
+
+    new_book = Book(book_title, book_author, book_genre)
+    books_in_library.append(new_book)
+
+    return render_template('index.jinja', library_book_list = books_in_library)
